1cdd833b83c58be09a4d51f56966ad0807356548
Oscar Lindberg <oscarlindberg@me.com>
2020-12-03 16:21:21 +0100

    context and example of adding an activity is implemented

--- App.js
+++ App.js
@@ -5,16 +5,31 @@ import {NativeRouter, Switch, Route } from 'react-router-native';
 import HomeScreen from './screens/HomeScreen';
 import NewActivityScreen from './screens/NewActivityScreen';
 
+import { ActivitiesContext } from './contexts'
+
+const exampleToDos = [
+  { activity: 'walk the cat', type: 'health', alert: true, alertWhen: '12:00'},
+  { activity: 'eat pizza', type: 'health', alert: true, alertWhen: '11:59'},
+  { activity: 'change dipers', type: 'family', alert: false, alertWhen: '20:00'},
+  { activity: 'hit boss in eye', type: 'work', alert: false, alertWhen: '00:00'}
+]
+
 export default function App() {
+
+  const [ activities, setActivities ] = React.useState(exampleToDos)
+  const activitiesProviderValue = React.useMemo(()=> ({ activities, setActivities }), [activities, setActivities])
+
   return (
-    <NativeRouter>
-      <SafeAreaView style={[styles.container, styles.AndroidSafeArea]}>
-        <Switch>
-          <Route exact path="/" component={HomeScreen} />
-          <Route path="/newActivity" component={NewActivityScreen} />
-        </Switch>
-      </SafeAreaView>
-    </NativeRouter>
+    <ActivitiesContext.Provider value={activitiesProviderValue}>
+      <NativeRouter>
+        <SafeAreaView style={[styles.container, styles.AndroidSafeArea]}>
+          <Switch>
+            <Route exact path="/" component={HomeScreen} />
+            <Route path="/newActivity" component={NewActivityScreen} />
+          </Switch>
+        </SafeAreaView>
+      </NativeRouter>
+    </ActivitiesContext.Provider>
   );
 }
 
