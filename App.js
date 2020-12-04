78881e7e15e4a68e0a33110e0458f3bbeb138b43
Ted Nilsson <tedn94@gmail.com>
2020-12-04 12:45:10 +0100

    Fixed basic code for themes

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, SafeAreaView, Platform } from 'react-native';
-import {NativeRouter, Switch, Route } from 'react-router-native';
+import { NativeRouter, Switch, Route } from 'react-router-native';
 import HomeScreen from './screens/HomeScreen';
 import NewActivityScreen from './screens/NewActivityScreen';
+import { AppearanceProvider } from 'react-native-appearance';
+import { ThemeProvider } from './themes/ThemeContext';
 
 export default function App() {
   return (
     <NativeRouter>
-      <SafeAreaView style={[styles.container, styles.AndroidSafeArea]}>
-        <Switch>
-          <Route exact path="/" component={HomeScreen} />
-          <Route path="/newActivity" component={NewActivityScreen} />
-        </Switch>
-      </SafeAreaView>
+      <AppearanceProvider>
+        <ThemeProvider>
+          <SafeAreaView style={[styles.container, styles.AndroidSafeArea]}>
+            <Switch>
+              <Route exact path="/" component={HomeScreen} />
+              <Route path="/newActivity" component={NewActivityScreen} />
+            </Switch>
+          </SafeAreaView>
+        </ThemeProvider>
+      </AppearanceProvider>
     </NativeRouter>
   );
 }
@@ -22,7 +28,7 @@ const styles = StyleSheet.create({
   container: {
     display: 'flex',
     flex: 1,
-    backgroundColor: '#1E2036',
+    // backgroundColor: '#1E2036',
     alignItems: 'flex-start',
     justifyContent: 'flex-start',
   },
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,17 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import  Header  from '../components/HeaderComponents/Header';
-import  AddButton  from '../components/ButtonComponents/AddButton';
+import Header from '../components/HeaderComponents/Header';
+import AddButton from '../components/ButtonComponents/AddButton';
+import { useTheme } from '../themes/ThemeContext';
 
+export default function HomeScreen(props) {
+  const { colors } = useTheme();
+  const { history } = props;
 
-export default function HomeScreen({history}) {
+  const containerStyle = {
+    backgroundColor: colors.background
+  };
 
   return (
-    <View style={styles.container}>
-      <Header title="Today's activities"/>
+    <View style={[containerStyle, styles.container]}>
+      <Header title="Today's activities" />
       <View style={styles.addButton}>
-        <AddButton history={history}/>
+        <AddButton history={history} />
       </View>
     </View>
   );
@@ -21,7 +27,7 @@ const styles = StyleSheet.create({
   container: {
     width: '100%',
     height: '100%',
-    paddingTop: 50
+    paddingTop: 50,
   },
   addButton: {
     position: "absolute",
