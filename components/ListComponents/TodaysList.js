0dad0ffd6190066bef029ffe14e449a5a7b5008c
Emma Ekmank <emma.ekman@fortnox.see>
2020-12-07 18:59:00 +0100

    renders time and notification icon

--- components/ListComponents/TodaysList.js
+++ components/ListComponents/TodaysList.js
@@ -1,10 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {
-  Animated,
   StyleSheet,
   Text,
   TouchableOpacity,
-  TouchableHighlight,
   View,
 } from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
@@ -13,63 +11,37 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import { SwipeListView } from 'react-native-swipe-list-view';
 import { ActivitiesContext } from '../../contexts'
 
-
-const rowSwipeAnimatedValues = {};
-// Array(20)
-//     .fill('')
-//     .forEach((_, i) => {
-//         rowSwipeAnimatedValues[`${i}`] = new Animated.Value(0);
-//     });
-//VAD GÖR DENNA
-
 export default function TodaysList() {
 
   const { activities, setActivities } = React.useContext(ActivitiesContext);
-  const [checked, setChecked] = useState(false);
-
-  // console.log('activities', activities)
-
-  // const onSwipeValueChange = swipeData => {
-  //   console.log('swipeData', swipeData);
-  //     // const { key, value } = swipeData;
-  //     // rowSwipeAnimatedValues[key].setValue(Math.abs(value));
-  // };
-
-  const editActivity = () => {
-
-  }
-
-  const deleteActivity = (rowMap, data) => {
-    console.log(rowMap, data.item)
-  }
 
   const renderItem = (data) => {
     return (
-      <View style={data.item.completed ? styles. rowFrontDone : styles.rowFront}>
-        
-          <View style={{display: 'flex', justifyContent: 'center', flexDirection: 'column'}}>
-            <Text style={styles.activityText}>{data.item.activity}</Text>
+      <View style={data.item.completed ? styles.listItemDone : styles.listItem}>
+        <View style={{display: 'flex', justifyContent: 'center', flexDirection: 'column'}}>
+          <Text style={styles.activityText}>{data.item.activity}</Text>
+          <View style={{display: 'flex', justifyContent: 'flex-start', flexDirection: 'row'}}>
             {data.item.alert ? 
-            <View style={{display: 'flex', justifyContent: 'flex-start', flexDirection: 'row'}}>
               <Ionicons style={styles.notifyIcon} name="ios-notifications" size={20} color="#EBB000"/>
+            : null}
+            {data.item.alertWhen ? 
               <Text
               style={{
-                color: data.item.completed ? '#fff' : '#85BCA9',
+                color: data.item.completed ? '#F5F4F8' : '#85BCA9',
                 fontSize: 12,
-                marginLeft: 10,
+                marginLeft: data.item.alert ? 10 : 20,
                 marginTop: 10
                 }}
                 >{data.item.alertWhen}</Text>
-              </View>
-              : null
-            }
+            : null}
+          </View>
         </View>
         <TouchableOpacity style={styles.checkIcon} onPress={() => setActivities({ type: 'SET_COMPLETED', payload: data.item.activity })}>
           {data.item.completed ?
-            <AntDesign name="checkcircleo" size={50} color="#fff" />
-            : <Feather name="circle" size={50} color="#85BCA9" />
+            <AntDesign name="checkcircleo" size={50} color="#F5F4F8" />
+            : 
+            <Feather name="circle" size={50} color="#85BCA9" />
           }
-          
         </TouchableOpacity>
       </View>
     )
@@ -78,14 +50,13 @@ export default function TodaysList() {
   const renderHiddenItem = (data, rowMap) => (
     <View style={styles.rowBack}>
       <TouchableOpacity
-        style={styles.leftButtonContainer}
+        style={styles.leftButton}
         onPress={() => editActivity(rowMap, data.key)}
       >
         <Text style={{ color: '#F5F4F8' }}>Edit</Text>
       </TouchableOpacity>
       <TouchableOpacity
-        style={[styles.backRightBtn, styles.backRightBtnRight]}
-        onPress={() => deleteActivity(rowMap, data.key)}
+        style={styles.backRightBtnRight}
         onPress={() => setActivities({ type: 'REMOVE_ACTIVITY', payload: data.item.activity })}
       >
         <Text style={{ color: "#F5F4F8" }}>Delete</Text>
@@ -104,9 +75,8 @@ export default function TodaysList() {
         previewRowKey={'0'}
         previewOpenValue={-40}
         previewOpenDelay={3000}
+        friction={4}
         keyExtractor={(item, index) => index.toString()}
-      // onRowDidOpen={onRowDidOpen}
-      // onSwipeValueChange={onSwipeValueChange}
       />
     </View>
   );
@@ -118,7 +88,7 @@ const styles = StyleSheet.create({
     width: '100%',
     paddingTop: 50,
   },
-  rowFront: {
+  listItem: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -128,9 +98,9 @@ const styles = StyleSheet.create({
     margin: 10,
     borderRadius: 15,
     backgroundColor: '#3f4155',
-    color: "#fff"
+    color: "#F5F4F8"
   },
-  rowFrontDone: {
+  listItemDone: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -140,7 +110,7 @@ const styles = StyleSheet.create({
     margin: 10,
     borderRadius: 15,
     backgroundColor: '#85BCA9',
-    color: "#fff"
+    color: "#F5F4F8"
   },
   rowBack: {
     alignItems: 'center',
@@ -153,7 +123,10 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     paddingLeft: 5,
   },
-  backRightBtn: {
+  backRightBtnRight: {
+    backgroundColor: '#EB5500',
+    right: 0,
+    borderRadius: 100,
     alignItems: 'center',
     bottom: 0,
     justifyContent: 'center',
@@ -161,29 +134,12 @@ const styles = StyleSheet.create({
     top: 0,
     width: 66,
   },
-  backRightBtnLeft: {
-    backgroundColor: '#5E6170',
-    right: 75,
-    borderRadius: 100,
-    color: 'red',
-  },
-  backRightBtnRight: {
-    backgroundColor: '#EB5500',
-    right: 0,
-    borderRadius: 100,
-  },
   activityText: {
     color: '#F4F7F8',
     fontSize: 20,
     marginLeft: 20,
   },
-  alertText: {
-    color: '#85BCA9',
-    fontSize: 12,
-    marginLeft: 10,
-    marginTop: 10
-  },
-  leftButtonContainer: {
+  leftButton: {
     borderRadius: 100,
     backgroundColor: '#5E6170',
     alignItems: 'center',
