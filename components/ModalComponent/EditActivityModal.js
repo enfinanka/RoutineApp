fd937bab5aea58a68e773bc33e061c843f72e5f9
Karwan Gulnezer <karwan_gulnezer@hotmail.com>
2020-12-08 13:09:16 +0100

    commiting for safty

--- components/ModalComponent/EditActivityModal.js
+++ components/ModalComponent/EditActivityModal.js
@@ -1,48 +1,83 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableHighlight, Modal, Alert} from 'react-native';
 import { TextInput } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/Ionicons';
+import SwitchToggle from '../ButtonComponents/SwitchToggle';
+import TimeButton from '../ButtonComponents/TimeButton';
+import UpdateActivityButton from '../ButtonComponents/UpdateActivityButton';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
+export default function EditActivityModal(props) {
+  
+  const [inputActivity, setInputActivity] = React.useState('');
+  const [show, setShow] = React.useState(false);
+  const [alert, setAlert] = React.useState(false);
+  const [chosenTime, setChosenTime] = React.useState('');
 
-export default function EditActivityModal() {
-    
+  const { setShowEditModal, showEditModal, activities, setActivities, activityName } = props;  
+
+  const activityAlreadyExists = () => activities.some((obj) => obj.activity === inputActivity)
+
+  const editActivity = () => {
+    // more valitators??
+    if (activityAlreadyExists()) {
+      Alert.alert(`You already have an activity called: ${inputActivity}`)
+    }
+    else {
+      const changeActivity = {
+        completed: false,
+        activity: inputActivity,
+        type: 'work',
+        alert: alert,
+        alertWhen: chosenTime
+      }
+      console.log(changeActivity)
+      setActivities({ type: 'CHANGE_ACTIVITY', payload: changeActivity })
+      setShowEditModal(false);
+      //history.push('/');
+    }
+  }
+
+  const handleConfirm = (time) => {
+    console.log(time, 'time');
+    let chosenTime = time.toLocaleTimeString().slice(0, 5);
+
+    setShow(false);
+    setChosenTime(chosenTime);
+  }
+
+  const hideDatePicker = () => {
+    setShow(false);
+  }
+
   return (
     <View style={styles.container}>
       <Modal
         animationType="slide"
         transparent={true}
-        visible={showModal}
+        visible={showEditModal}
         onRequestClose={() => {
-          setShowModal(false);
+          setShowEditModal(false);
         }}
       >
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
-
+            
             <TouchableHighlight
               style={styles.closeButton}
               onPress={() => {
-                setShowModal(false);
+                setShowEditModal(false);
               }}>
-
               <Icon name="ios-close" size={50} color="#F4F7F8" />
             </TouchableHighlight>
-
-            <Text style={styles.modalHeader}>New activity</Text>
-            <View style={styles.container}>
-              <TextInput
-                label="Type your Activity"
-                style={styles.input}
-                onChangeText={text => setInputActivity(text)}
-                value={inputActivity}
-              />
-              <TextInput
-                label="Category"
-                style={styles.input}
-                onChangeText={text => setInputCategory(text)}
-                value={inputCategory}
-              />
-            </View>
-            {/* <Input /> */}
+              
+            <TextInput style={styles.modalHeader}
+              label={activityName}
+              onChangeText={text => setInputActivity(text)}
+              value={inputActivity}
+              theme={{ colors: {background: '#5E6170', text: '#fff' } }}
+            />
+            
             <View style={styles.textContainer}>
               <Text style={styles.modalText} >Notifications</Text>
               <SwitchToggle setAlert={setAlert} alert={alert} />
@@ -58,12 +93,12 @@ export default function EditActivityModal() {
                   mode="time"
                   onConfirm={handleConfirm}
                   onCancel={hideDatePicker}
-                  locale="en_GB"
+                  locale="gb" // Use "en_GB" here
                   is24Hour={true}
                 />
               </View>
               : null}
-            <AddActivityButton history={history} addNewActivity={addNewActivity} setShowModal={setShowModal} />
+            <UpdateActivityButton editActivity={editActivity} />
           </View>
         </View>
       </Modal>
@@ -72,14 +107,61 @@ export default function EditActivityModal() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-   paddingBottom: 50,
+  centeredView: {
+    flex: 1,
+    justifyContent: 'flex-end',
   },
-  input: {
-   height: 50,
-   margin: 10,
-   width: 350,
-   color: '#fff',
-   textAlign: 'center',
+  modalView: {
+    display: 'flex',
+    alignItems: 'center',
+    backgroundColor: "white",
+    borderTopRightRadius: 50,
+    borderTopLeftRadius: 50,
+    padding: 100,
+    backgroundColor: '#5E6170',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 10
+  },
+  closeButton: {
+    backgroundColor: '#EB7100',
+    borderRadius: 50,
+    width: 70,
+    padding: 10,
+    position: "absolute",
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: -50,
+  },
+  modalHeader: {
+    position: "absolute",
+    margin: 25,
+    fontSize: 35,
+    textAlign: "center",
+    color: '#F4F7F8',
+    width: 300,
+  },
+  modalText: {
+    color: '#F4F7F8',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  textContainer: {
+    margin: 10,
+    marginTop: 30,
+    width: 300,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignContent: 'space-around',
+  },
+  container: {
+    paddingBottom: 50,
   },
 });
