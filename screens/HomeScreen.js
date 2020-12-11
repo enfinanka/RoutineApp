2ef101d2017e5ca8174cc33cd7c3a9bbda35cf08
Oscar Lindberg <oscarlindberg@me.com>
2020-12-11 12:48:16 +0100

    all sorting on todays list done

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -13,9 +13,49 @@ export default function HomeScreen({ history }) {
   const [refresh, setRefresh] = React.useState(false)
 
   const sortAndSetActivities = (d) => {
+
     const notCompleted = d.filter((a) => !a.completed)
     const completed = d.filter((a) => a.completed)
-    const sorted = [...notCompleted, ...completed ]
+
+    const completedAndHasTime = completed.filter((a)=> a.alert)
+    const notCompletedAndHasTime = notCompleted.filter((a)=> a.alert)
+
+    const notCompletedNoTime = notCompleted.filter((a)=> !a.alert)
+    const completedNoTime = completed.filter((a)=> !a.alert)
+
+    const notCompletedAndHasTimeAsNumber = notCompletedAndHasTime.map((a)=> {
+      const timeAsNumberWOColon = parseInt(a.alertWhen.replace(':','')) 
+      return {...a, alertWhen: timeAsNumberWOColon}
+    })
+
+    const completedAndHasTimeAsNumber = completedAndHasTime.map((a)=> {
+      const timeAsNumberWOColon = parseInt(a.alertWhen.replace(':','')) 
+      return {...a, alertWhen: timeAsNumberWOColon}
+    })
+
+    const notCompletedSortedByTime = notCompletedAndHasTimeAsNumber.sort((a,b) => a.alertWhen - b.alertWhen) 
+    const completedSortedByTime = completedAndHasTimeAsNumber.sort((a,b) => a.alertWhen - b.alertWhen) 
+
+    const notCompletedSortedByTimeAsString = notCompletedSortedByTime.map((a)=> {
+      let timeAsString = a.alertWhen.toString()
+      if(timeAsString.length < 4) {
+        timeAsString = 0 + timeAsString
+      }
+      timeAsString = timeAsString.substring(0,2) + ":" + timeAsString.substring(2, timeAsString.length)
+      return {...a, alertWhen: timeAsString}
+    })
+
+    const completedSortedByTimeAsString = completedSortedByTime.map((a)=> {
+      let timeAsString = a.alertWhen.toString()
+      if(timeAsString.length < 4) {
+        timeAsString = 0 + timeAsString
+      }
+      timeAsString = timeAsString.substring(0,2) + ":" + timeAsString.substring(2, timeAsString.length)
+      return {...a, alertWhen: timeAsString}
+    })
+
+    const sorted = [...notCompletedSortedByTimeAsString, ...notCompletedNoTime, ...completedSortedByTimeAsString, ...completedNoTime ]
+    
     setActivities({ type: 'ADD_FROM_ASYNCSTORAGE', payload: sorted })
   }
 
