ff232959401434336521761f7fcd9f1958ce93a7
Oscar Lindberg <oscarlindberg@me.com>
2020-12-14 11:20:14 +0100

    cleaned up homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,7 +4,7 @@ import Header from '../components/HeaderComponents/Header';
 import TodaysList from '../components/ListComponents/TodaysList';
 import { ActivitiesContext } from '../contexts';
 import NoActivities from '../components/ListComponents/NoActivities';
-import { retrieveDataFromAsyncStorage, InitalStoreDataToAsyncStorage, clearAllAsyncStorage } from '../utils/asyncStorage';
+import { retrieveDataFromAsyncStorage } from '../utils/asyncStorage';
 import { sortActivities } from '../utils/sortingActivities';
 import AddButton from '../components/ButtonComponents/AddButton';
 
@@ -13,11 +13,8 @@ export default function HomeScreen({ history }) {
   const { activities, setActivities } = React.useContext(ActivitiesContext);
   const [refresh, setRefresh] = React.useState(false)
 
-  //kommentera fram InitalStoreDataToAsyncStorage() för att lägga in exemplen i asyncStorage.
   React.useEffect(() => {
     const timer = setTimeout(() => {
-      // InitalStoreDataToAsyncStorage()
-      // clearAllAsyncStorage()
       retrieveDataFromAsyncStorage()
         .then((d) => setActivities({ type: 'ADD_FROM_ASYNCSTORAGE', payload: sortActivities(d) }))
     }, 0)
